fa648d1f299f2b8636a6d76ebb3f35a1189ba8d0
thiagocristhianferreira <thiagocristhian@gmail.com>
2022-07-09 14:25:52 -0300

    checkStyle

--- src/main/java/app/trybe/specialityapp/service/ProfessionalService.java
+++ src/main/java/app/trybe/specialityapp/service/ProfessionalService.java
@@ -2,12 +2,14 @@ package app.trybe.specialityapp.service;
 
 import app.trybe.specialityapp.model.Professional;
 import app.trybe.specialityapp.repository.ProfessionalRepository;
+import java.util.List;
 import org.jvnet.hk2.annotations.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
+/**
+ * ProfessionalService.
+ */
 @Service
 @Component
 public class ProfessionalService {
