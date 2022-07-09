e81798b4496e7491c21780baa08b65d711db764d
thiagocristhianferreira <thiagocristhian@gmail.com>
2022-07-09 14:08:29 -0300

    ajustando SpecialityAppApplication

--- src/main/java/app/trybe/specialityapp/SpecialityAppApplication.java
+++ src/main/java/app/trybe/specialityapp/SpecialityAppApplication.java
@@ -1,20 +1,15 @@
 package app.trybe.specialityapp;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
-//@Configuration
-//@EnableAutoConfiguration
 @SpringBootApplication
-@ComponentScan(basePackages = {"app.trybe.specialityapp"})
+@SpringBootConfiguration
 public class SpecialityAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpecialityAppApplication.class, args);
+		System.out.println("starting aplication");
 	}
 
 }
