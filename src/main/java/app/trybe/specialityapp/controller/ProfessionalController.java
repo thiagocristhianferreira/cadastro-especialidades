08a9b31a89b1025613ef2bc40700ee2d7036a6eb
thiagocristhianferreira <thiagocristhian@gmail.com>
2022-07-09 19:09:41 -0300

    refatorando usando ApplicationError

--- src/main/java/app/trybe/specialityapp/controller/ProfessionalController.java
+++ src/main/java/app/trybe/specialityapp/controller/ProfessionalController.java
@@ -1,15 +1,17 @@
 package app.trybe.specialityapp.controller;
 
+import app.trybe.specialityapp.commons.ApplicationError;
 import app.trybe.specialityapp.model.Professional;
 import app.trybe.specialityapp.service.ProfessionalService;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+
+import javax.validation.Valid;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -24,24 +26,46 @@ public class ProfessionalController {
   private ProfessionalService service;
 
   /**
-   * findAll.
+   * Pega todos os registros.
    */
   @GET
   @Path("/all")
   @Consumes("application/json")
   @Produces("application/json")
-  public Response findAll() {
+  public Response pegaTodosRegistros() {
     try {
-      // service.getAllProfessionals().get(0);
       return Response
           .status(Response.Status.OK)
-          .entity(service.getAllProfessionals())
+          .entity(service.pegaTudo())
           .build();
-    } catch (IndexOutOfBoundsException e) {
+    } catch (ApplicationError e) {
       return Response
-          .status(Response.Status.NOT_FOUND)
-          .entity("Nenhum registro foi encontrado!")
+          .status(e.getStatus())
+          .entity(e.getMessage())
           .build();
     }
   }
+
+  /**
+   * Cadastra registros.
+   */
+  @POST
+  @Path("/add")
+  @Consumes("application/json")
+  @Produces("application/json")
+  public Response cadastra(@Valid @RequestBody Professional professional) {
+    try {
+      service.adicionaRegistro(professional);
+      return Response
+          .status(Response.Status.CREATED)
+          .entity("Inserido")
+          .build();
+    } catch (ApplicationError e) {
+      return Response
+          .status(e.getStatus())
+          .entity(e.getMessage())
+          .build();
+    }
+  }
+
 }
--- src/main/java/app/trybe/specialityapp/service/ProfessionalService.java
+++ src/main/java/app/trybe/specialityapp/service/ProfessionalService.java
@@ -1,10 +1,12 @@
 package app.trybe.specialityapp.service;
 
+import app.trybe.specialityapp.commons.ApplicationError;
 import app.trybe.specialityapp.model.Professional;
 import app.trybe.specialityapp.repository.ProfessionalRepository;
 import java.util.List;
 import org.jvnet.hk2.annotations.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContextException;
 import org.springframework.stereotype.Component;
 
 import javax.validation.constraints.AssertTrue;
@@ -21,8 +23,18 @@ public class ProfessionalService {
   @Autowired
   private ProfessionalRepository repository;
 
-  public List<Professional> getAllProfessionals() {
+  public List<Professional> pegaTudo() throws ApplicationError {
+    if (repository.findAll().size() == 0) {
+      throw new ApplicationError(404, "Nenhum registro foi encontrado!");
+    }
     return repository.findAll();
+  }
+
+  public Professional adicionaRegistro (Professional professional) throws ApplicationError {
+    if (professional.getId() != null) {
+      throw new ApplicationError(400, "Não é permitido inserir novos registros com ID explícito.");
+    }
+    return repository.save(professional);
   }
 
 }
