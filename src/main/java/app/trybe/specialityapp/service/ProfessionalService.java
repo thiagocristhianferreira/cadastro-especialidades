3aaa949384d4fd41260e364eaa48fab704f5c657
thiagocristhianferreira <thiagocristhian@gmail.com>
2022-07-10 01:56:28 -0300

    Ajuste ProfessionalService

--- src/main/java/app/trybe/specialityapp/service/ProfessionalService.java
+++ src/main/java/app/trybe/specialityapp/service/ProfessionalService.java
@@ -4,6 +4,7 @@ import app.trybe.specialityapp.commons.ApplicationError;
 import app.trybe.specialityapp.model.Professional;
 import app.trybe.specialityapp.repository.ProfessionalRepository;
 import java.util.List;
+import javax.ws.rs.core.Response;
 import org.jvnet.hk2.annotations.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -22,7 +23,7 @@ public class ProfessionalService {
    */
   public List<Professional> pegaTudo() throws ApplicationError {
     if (repository.findAll().size() == 0) {
-      throw new ApplicationError(404, "Nenhum registro foi encontrado!");
+      throw new ApplicationError("Nenhum registro foi encontrado!", Response.Status.NOT_FOUND);
     }
     return repository.findAll();
   }
@@ -32,7 +33,9 @@ public class ProfessionalService {
    */
   public Professional adicionaRegistro(Professional professional) throws ApplicationError {
     if (professional.getId() != null) {
-      throw new ApplicationError(400, "Não é permitido inserir novos registros com ID explícito.");
+      throw new ApplicationError(
+          "Não é permitido inserir novos registros com ID explícito.",
+          Response.Status.BAD_REQUEST);
     }
     return repository.save(professional);
   }
