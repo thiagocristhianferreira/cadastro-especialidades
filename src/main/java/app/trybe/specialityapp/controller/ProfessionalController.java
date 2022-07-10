3ca8d98a80f5e746c536ff99feaeffc9f4b33c74
thiagocristhianferreira <thiagocristhian@gmail.com>
2022-07-10 13:34:10 -0300

    checkStyle

--- src/main/java/app/trybe/specialityapp/controller/ProfessionalController.java
+++ src/main/java/app/trybe/specialityapp/controller/ProfessionalController.java
@@ -3,8 +3,14 @@ package app.trybe.specialityapp.controller;
 import app.trybe.specialityapp.commons.ApplicationError;
 import app.trybe.specialityapp.model.Professional;
 import app.trybe.specialityapp.service.ProfessionalService;
-
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
